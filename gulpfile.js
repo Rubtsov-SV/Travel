0c0f8c8891b719741e337752c527ebd5555dc680
Сергей <serega16.92@inbox.ru>
2023-09-03 13:42:50 +0500

    Правки после код ревью

--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,7 @@ const syncServer = () => {
   gulp.watch('source/video/**', gulp.series(copy, refresh));
   gulp.watch('source/downloads/**', gulp.series(copy, refresh));
   gulp.watch('source/*.php', gulp.series(copy, refresh));
+  gulp.watch('source/**.webmanifest', gulp.series(copy, refresh));
 };
 
 const refresh = (done) => {
