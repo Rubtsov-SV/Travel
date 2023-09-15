7ec5dc9d1be5a01cefa4759cd9636572170199cc
Сергей <serega16.92@inbox.ru>
2023-09-15 18:01:34 +0500

    Правки баг теста

--- source/js/main.js
+++ source/js/main.js
@@ -49,97 +49,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // используйте .closest(el)
 
-// Скрол
-const bannerScrollTuors = document.querySelectorAll('.banner-scroll-tours');
-const tours = document.querySelector('#tours');
-
-if (bannerScrollTuors) {
-  for (const bannerScrollTuor of bannerScrollTuors) {
-    bannerScrollTuor.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      tours.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
-    });
-  }
-}
-
-const bannerScrollTrainings = document.querySelectorAll('.banner-scroll-training');
-const training = document.querySelector('#training');
-
-if (bannerScrollTrainings) {
-  for (const bannerScrollTraining of bannerScrollTrainings) {
-    bannerScrollTraining.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      training.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
-    });
-  }
-}
-
-const bannerScrollAboutUss = document.querySelectorAll('.banner-scroll-about-us');
-const aboutUs = document.querySelector('#about-us');
-
-if (bannerScrollAboutUss) {
-  for (const bannerScrollAboutUs of bannerScrollAboutUss) {
-    bannerScrollAboutUs.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      aboutUs.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
-    });
-  }
-}
-
-const bannerScrollFeedbacks = document.querySelectorAll('.banner-scroll-feedback');
-const feedback = document.querySelector('#feedback');
-
-if (bannerScrollFeedbacks) {
-  for (const bannerScrollFeedback of bannerScrollFeedbacks) {
-    bannerScrollFeedback.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      feedback.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
-    });
-  }
-}
-
-const bannerScrollPhotogallerys = document.querySelectorAll('.banner-scroll-photogallery');
-const photogallery = document.querySelector('#photogallery');
-
-if (bannerScrollPhotogallerys) {
-  for (const bannerScrollPhotogallery of bannerScrollPhotogallerys) {
-    bannerScrollPhotogallery.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      photogallery.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
-    });
-  }
-}
-
-const bannerScrollContactss = document.querySelectorAll('.banner-scroll-contacts');
-const contacts = document.querySelector('#contacts');
-
-if (bannerScrollContactss) {
-  for (const bannerScrollContacts of bannerScrollContactss) {
-    bannerScrollContacts.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      contacts.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
-    });
-  }
-}
-
 // Слайдер Hero
 
 const swiperHero = new Swiper('.hero__swiper', {
@@ -152,111 +61,427 @@ const swiperHero = new Swiper('.hero__swiper', {
 });
 
 // Бургер
+const body = document.body;
 
-const button1 = document.querySelector('.header__burger1');
+const buttonBoxs1 = document.querySelectorAll('.header__box1');
+const backgrounds = document.querySelectorAll('.hero__overlay-close');
+// const itemLinks = document.querySelectorAll('.hero__item-link');
 
+for (const buttonBox of buttonBoxs1) {
+  const button = buttonBox.querySelector('.header__burger1');
+  const menu = buttonBox.querySelector('.header__list1');
+  const phone = buttonBox.querySelector('.header__phone1');
+  const logo = buttonBox.querySelector('.logo1');
 
-const menu1 = document.querySelector('.header__list1');
-const phone1 = document.querySelector('.header__phone1');
-const logo1 = document.querySelector('.logo1');
-const box1 = document.querySelector('.header__box1');
+  const open = () => {
+    button.classList.remove('header__burger-close');
+    button.classList.add('header__burger-open');
+    menu.style.display = 'flex';
+    phone.style.color = '#000000';
+    logo.src = '/img/logo-black.png';
+    buttonBox.style.background = '#ffffff';
+    body.style.overflow = 'hidden';
+    for (const background of backgrounds) {
+      background.classList.add('hero__overlay-open');
+    }
+  };
 
-const open1 = () => {
-  button1.classList.remove('header__burger-close');
-  button1.classList.add('header__burger-open');
-  menu1.style.display = 'flex';
-  phone1.style.color = '#000000';
-  logo1.src = '/img/logo-black.png';
-  box1.style.background = '#ffffff';
-};
+  const close = () => {
+    button.classList.remove('header__burger-open');
+    button.classList.add('header__burger-close');
+    menu.style.display = 'none';
+    phone.style.color = '#ffffff';
+    logo.src = '/img/logo.png';
+    buttonBox.style.background = '#ffffff00';
+    body.style.overflow = 'visible';
+    for (const background of backgrounds) {
+      background.classList.remove('hero__overlay-open');
+    }
+  };
 
-const close1 = () => {
-  button1.classList.remove('header__burger-open');
-  button1.classList.add('header__burger-close');
-  menu1.style.display = 'none';
-  phone1.style.color = '#ffffff';
-  logo1.src = '/img/logo.png';
-  box1.style.background = '#ffffff00';
-};
+  const bannerScrollTuors = document.querySelectorAll('.banner-scroll-tours');
+  const tours = document.querySelector('#tours');
 
-
-button1.addEventListener('click', ()=> {
-  if (button1.classList.contains('header__burger-close')) {
-    open1();
-  } else {
-    close1();
+  if (bannerScrollTuors) {
+    for (const bannerScrollTuor of bannerScrollTuors) {
+      bannerScrollTuor.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        tours.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
   }
-});
 
-const button2 = document.querySelector('.header__burger2');
+  const bannerScrollTrainings = document.querySelectorAll('.banner-scroll-training');
+  const training = document.querySelector('#training');
 
-
-const menu2 = document.querySelector('.header__list2');
-const phone2 = document.querySelector('.header__phone2');
-const logo2 = document.querySelector('.logo2');
-const box2 = document.querySelector('.header__box2');
-
-const open2 = () => {
-  button2.classList.remove('header__burger-close');
-  button2.classList.add('header__burger-open');
-  menu2.style.display = 'flex';
-  phone2.style.color = '#000000';
-  logo2.src = '/img/logo-black.png';
-  box2.style.background = '#ffffff';
-};
-
-const close2 = () => {
-  button2.classList.remove('header__burger-open');
-  button2.classList.add('header__burger-close');
-  menu2.style.display = 'none';
-  phone2.style.color = '#ffffff';
-  logo2.src = '/img/logo.png';
-  box2.style.background = '#ffffff00';
-};
-
-
-button2.addEventListener('click', ()=> {
-  if (button2.classList.contains('header__burger-close')) {
-    open2();
-  } else {
-    close2();
+  if (bannerScrollTrainings) {
+    for (const bannerScrollTraining of bannerScrollTrainings) {
+      bannerScrollTraining.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        training.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
   }
-});
 
-const button3 = document.querySelector('.header__burger3');
+  const bannerScrollAboutUss = document.querySelectorAll('.banner-scroll-about-us');
+  const aboutUs = document.querySelector('#about-us');
 
-
-const menu3 = document.querySelector('.header__list3');
-const phone3 = document.querySelector('.header__phone3');
-const logo3 = document.querySelector('.logo3');
-const box3 = document.querySelector('.header__box3');
-
-const open3 = () => {
-  button3.classList.remove('header__burger-close');
-  button3.classList.add('header__burger-open');
-  menu3.style.display = 'flex';
-  phone3.style.color = '#000000';
-  logo3.src = '/img/logo-black.png';
-  box3.style.background = '#ffffff';
-};
-
-const close3 = () => {
-  button3.classList.remove('header__burger-open');
-  button3.classList.add('header__burger-close');
-  menu3.style.display = 'none';
-  phone3.style.color = '#ffffff';
-  logo3.src = '/img/logo.png';
-  box3.style.background = '#ffffff00';
-};
-
-
-button3.addEventListener('click', ()=> {
-  if (button3.classList.contains('header__burger-close')) {
-    open3();
-  } else {
-    close3();
+  if (bannerScrollAboutUss) {
+    for (const bannerScrollAboutUs of bannerScrollAboutUss) {
+      bannerScrollAboutUs.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        aboutUs.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
   }
-});
+
+  const bannerScrollFeedbacks = document.querySelectorAll('.banner-scroll-feedback');
+  const feedback = document.querySelector('#feedback');
+
+  if (bannerScrollFeedbacks) {
+    for (const bannerScrollFeedback of bannerScrollFeedbacks) {
+      bannerScrollFeedback.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        feedback.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
+  }
+
+  const bannerScrollPhotogallerys = document.querySelectorAll('.banner-scroll-photogallery');
+  const photogallery = document.querySelector('#photogallery');
+
+  if (bannerScrollPhotogallerys) {
+    for (const bannerScrollPhotogallery of bannerScrollPhotogallerys) {
+      bannerScrollPhotogallery.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        photogallery.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
+  }
+
+  const bannerScrollContactss = document.querySelectorAll('.banner-scroll-contacts');
+  const contacts = document.querySelector('#contacts');
+
+  if (bannerScrollContactss) {
+    for (const bannerScrollContacts of bannerScrollContactss) {
+      bannerScrollContacts.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        contacts.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
+  }
+
+  button.addEventListener('click', ()=> {
+    if (button.classList.contains('header__burger-close')) {
+      open();
+    } else {
+      close();
+    }
+  });
+}
+
+const buttonBoxs2 = document.querySelectorAll('.header__box2');
+
+for (const buttonBox of buttonBoxs2) {
+  const button = buttonBox.querySelector('.header__burger2');
+  const menu = buttonBox.querySelector('.header__list2');
+  const phone = buttonBox.querySelector('.header__phone2');
+  const logo = buttonBox.querySelector('.logo2');
+
+  const open = () => {
+    button.classList.remove('header__burger-close');
+    button.classList.add('header__burger-open');
+    menu.style.display = 'flex';
+    phone.style.color = '#000000';
+    logo.src = '/img/logo-black.png';
+    buttonBox.style.background = '#ffffff';
+    body.style.overflow = 'hidden';
+    for (const background of backgrounds) {
+      background.classList.add('hero__overlay-open');
+    }
+  };
+
+  const close = () => {
+    button.classList.remove('header__burger-open');
+    button.classList.add('header__burger-close');
+    menu.style.display = 'none';
+    phone.style.color = '#ffffff';
+    logo.src = '/img/logo.png';
+    buttonBox.style.background = '#ffffff00';
+    body.style.overflow = 'visible';
+    for (const background of backgrounds) {
+      background.classList.remove('hero__overlay-open');
+    }
+  };
+
+  const bannerScrollTuors = document.querySelectorAll('.banner-scroll-tours');
+  const tours = document.querySelector('#tours');
+
+  if (bannerScrollTuors) {
+    for (const bannerScrollTuor of bannerScrollTuors) {
+      bannerScrollTuor.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        tours.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
+  }
+
+  const bannerScrollTrainings = document.querySelectorAll('.banner-scroll-training');
+  const training = document.querySelector('#training');
+
+  if (bannerScrollTrainings) {
+    for (const bannerScrollTraining of bannerScrollTrainings) {
+      bannerScrollTraining.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        training.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
+  }
+
+  const bannerScrollAboutUss = document.querySelectorAll('.banner-scroll-about-us');
+  const aboutUs = document.querySelector('#about-us');
+
+  if (bannerScrollAboutUss) {
+    for (const bannerScrollAboutUs of bannerScrollAboutUss) {
+      bannerScrollAboutUs.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        aboutUs.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
+  }
+
+  const bannerScrollFeedbacks = document.querySelectorAll('.banner-scroll-feedback');
+  const feedback = document.querySelector('#feedback');
+
+  if (bannerScrollFeedbacks) {
+    for (const bannerScrollFeedback of bannerScrollFeedbacks) {
+      bannerScrollFeedback.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        feedback.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
+  }
+
+  const bannerScrollPhotogallerys = document.querySelectorAll('.banner-scroll-photogallery');
+  const photogallery = document.querySelector('#photogallery');
+
+  if (bannerScrollPhotogallerys) {
+    for (const bannerScrollPhotogallery of bannerScrollPhotogallerys) {
+      bannerScrollPhotogallery.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        photogallery.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
+  }
+
+  const bannerScrollContactss = document.querySelectorAll('.banner-scroll-contacts');
+  const contacts = document.querySelector('#contacts');
+
+  if (bannerScrollContactss) {
+    for (const bannerScrollContacts of bannerScrollContactss) {
+      bannerScrollContacts.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        contacts.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
+  }
+
+  button.addEventListener('click', ()=> {
+    if (button.classList.contains('header__burger-close')) {
+      open();
+    } else {
+      close();
+    }
+  });
+}
+
+
+const buttonBoxs3 = document.querySelectorAll('.header__box3');
+
+for (const buttonBox of buttonBoxs3) {
+  const button = buttonBox.querySelector('.header__burger3');
+  const menu = buttonBox.querySelector('.header__list3');
+  const phone = buttonBox.querySelector('.header__phone3');
+  const logo = buttonBox.querySelector('.logo3');
+
+  const open = () => {
+    button.classList.remove('header__burger-close');
+    button.classList.add('header__burger-open');
+    menu.style.display = 'flex';
+    phone.style.color = '#000000';
+    logo.src = '/img/logo-black.png';
+    buttonBox.style.background = '#ffffff';
+    body.style.overflow = 'hidden';
+    for (const background of backgrounds) {
+      background.classList.add('hero__overlay-open');
+    }
+  };
+
+  const close = () => {
+    button.classList.remove('header__burger-open');
+    button.classList.add('header__burger-close');
+    menu.style.display = 'none';
+    phone.style.color = '#ffffff';
+    logo.src = '/img/logo.png';
+    buttonBox.style.background = '#ffffff00';
+    body.style.overflow = 'visible';
+    for (const background of backgrounds) {
+      background.classList.remove('hero__overlay-open');
+    }
+  };
+
+  const bannerScrollTuors = document.querySelectorAll('.banner-scroll-tours');
+  const tours = document.querySelector('#tours');
+
+  if (bannerScrollTuors) {
+    for (const bannerScrollTuor of bannerScrollTuors) {
+      bannerScrollTuor.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        tours.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
+  }
+
+  const bannerScrollTrainings = document.querySelectorAll('.banner-scroll-training');
+  const training = document.querySelector('#training');
+
+  if (bannerScrollTrainings) {
+    for (const bannerScrollTraining of bannerScrollTrainings) {
+      bannerScrollTraining.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        training.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
+  }
+
+  const bannerScrollAboutUss = document.querySelectorAll('.banner-scroll-about-us');
+  const aboutUs = document.querySelector('#about-us');
+
+  if (bannerScrollAboutUss) {
+    for (const bannerScrollAboutUs of bannerScrollAboutUss) {
+      bannerScrollAboutUs.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        aboutUs.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
+  }
+
+  const bannerScrollFeedbacks = document.querySelectorAll('.banner-scroll-feedback');
+  const feedback = document.querySelector('#feedback');
+
+  if (bannerScrollFeedbacks) {
+    for (const bannerScrollFeedback of bannerScrollFeedbacks) {
+      bannerScrollFeedback.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        feedback.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
+  }
+
+  const bannerScrollPhotogallerys = document.querySelectorAll('.banner-scroll-photogallery');
+  const photogallery = document.querySelector('#photogallery');
+
+  if (bannerScrollPhotogallerys) {
+    for (const bannerScrollPhotogallery of bannerScrollPhotogallerys) {
+      bannerScrollPhotogallery.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        photogallery.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
+  }
+
+  const bannerScrollContactss = document.querySelectorAll('.banner-scroll-contacts');
+  const contacts = document.querySelector('#contacts');
+
+  if (bannerScrollContactss) {
+    for (const bannerScrollContacts of bannerScrollContactss) {
+      bannerScrollContacts.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        contacts.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        close();
+      });
+    }
+  }
+
+  button.addEventListener('click', ()=> {
+    if (button.classList.contains('header__burger-close')) {
+      open();
+    } else {
+      close();
+    }
+  });
+}
 
 // Видео экскурсия
 
